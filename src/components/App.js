e9257a18ef61073ed97e646ce941e6065ffc0643
Ryan Pearson <rpearson1994@icloud.com>
2019-10-13 16:17:20 -0400

    Added Search bar and css.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+import SearchBar from '../containers/search_bar';
+
 export default class App extends Component {
   render() {
     return (
       <div>
-        <p>Place Holder</p>
+        <div className='search-bar'>
+          <SearchBar />
+        </div>
       </div>
     );
   }
